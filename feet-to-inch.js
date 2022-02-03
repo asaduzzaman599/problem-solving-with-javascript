cebbdf7ccf05cf656a50f2626daf25981a81d68f
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-02-03 15:50:38 +0600

    feet to inch and centimeter to meter conversion added

--- feet-to-inch.js
+++ feet-to-inch.js
@@ -8,3 +8,15 @@ function feetToInch(feet){
 }
 const feet = 2;                                            //const feet = 2;
 console.log(`${feet} feet = ${feetToInch(feet)} inch`);    //output : 2 feet = 24 inch
+
+
+// write a function that takes Centimeter as parameter and return Meter.
+
+function centimeterToMeter(centimeter){
+    if(typeof centimeter != "number")return invalid;
+    let meter = centimeter / 100;
+    return meter;
+    //return centimeter / 100;
+}
+const centimeter = 2;                                                                //const centimeter = 2;
+console.log(`${centimeter} centimeter = ${centimeterToMeter(centimeter)} meter`);    //output : 2 centimeter = 0.02 meter
